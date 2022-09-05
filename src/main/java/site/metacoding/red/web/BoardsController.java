813af192156292b7a86ac276c900080105e3d7cf
Jo-HyeRin <sayhyelin12@naver.com>
2022-09-05 12:10:08 +0900

    view를 DTO로 변경함 - 코드 리팩토링

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -9,14 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import lombok.RequiredArgsConstructor;
-import site.metacoding.red.domain.boards.Boards;
 import site.metacoding.red.domain.boards.BoardsDao;
-import site.metacoding.red.domain.boards.mapper.MainView;
 import site.metacoding.red.domain.users.Users;
-import site.metacoding.red.domain.users.UsersDao;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
+import site.metacoding.red.web.dto.response.boards.MainDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,7 +23,7 @@ public class BoardsController {
 	private final HttpSession session;
 	//@PostMapping("/boards/{id}/delete")
 	//@PostMapping("/boards/{id}/update")
-
+	
 	@PostMapping("/boards")
 	public String writeBoards(WriteDto writeDto) {
 		// 1번 세션에 접근해서 세션값을 확인한다. 그때 Users로 다운캐스팅하고 키값은 principal로 한다.
@@ -47,13 +44,14 @@ public class BoardsController {
 		
 	@GetMapping({"/", "/boards"})
 	public String getBoardList(Model model) {
-		List<MainView> boardsList = boardsDao.findAll();
+		List<MainDto> boardsList = boardsDao.findAll();
 		model.addAttribute("boardsList", boardsList);
 		return "boards/main";
 	}
 	
 	@GetMapping("/boards/{id}")
-	public String getBoardList(@PathVariable Integer id) {
+	public String getBoardList(@PathVariable Integer id, Model model) {
+		model.addAttribute("boards", boardsDao.findById(id));
 		return "boards/detail";
 	}
 	
@@ -62,10 +60,8 @@ public class BoardsController {
 		Users principal = (Users)session.getAttribute("principal"); // 세션의 값을 가지고 올 때는 다운캐스팅해서 가져온다. 
 		if(principal == null) { // 로그인이 안 된 경우,  반복되는 필터링 메서드는 따로 빼서 모아 이용. 
 			return "redirect:/loginForm";
-		}
-		
-		return "boards/writeForm";
-		
-	}
+		}		
+		return "boards/writeForm";		
+	}	
 	
 }
--- src/main/java/site/metacoding/red/web/dto/response/boards/MainDto.java
+++ src/main/java/site/metacoding/red/web/dto/response/boards/MainDto.java
@@ -1,11 +1,11 @@
-package site.metacoding.red.domain.boards.mapper;
+package site.metacoding.red.web.dto.response.boards;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class MainView { // DAO와 DB 통신에 필요한 Entity, View
+public class MainDto {
 	private Integer id;
 	private String title;
 	private String username;
